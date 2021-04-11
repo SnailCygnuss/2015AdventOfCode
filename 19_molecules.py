78f181795d6df63247860def3e2a7b4bc6528bee
SnailCygnuss <sahil.a654@gmail.com>
2021-04-11 16:59:21 +0200

    Day 19 part 2

--- 19_molecules.py
+++ 19_molecules.py
@@ -1,5 +1,6 @@
 import time
 import re
+import random
 
 
 def read_file(f_name='Day_19/19_input.txt'):
@@ -31,13 +32,67 @@ def replace_mol(repl_list, medicine):
             after = medicine[m.end():]
             new_med = before + repl + after
             new_med_list.append(new_med)
-    
+
     return new_med_list
 
 
+def find_steps(repl_list, target_med):
+    """Find nnumber of steps to go from 'e' to target_med.
+    Slow and does not work!!!"""
+    med_len = len(target_med)
+    step = 1
+
+    set_new_med_list = set(replace_mol(repl_list, 'e'))
+
+    while target_med not in set_new_med_list:
+        step += 1
+        new_med_list = []
+
+        for new_med in set_new_med_list:
+            repl_med_list = replace_mol(repl_list, new_med)
+            new_med_list.extend(repl_med_list)
+
+        set_new_med_list = set(new_med_list)
+
+        filtered_med_list = []
+        for new_med in set_new_med_list:
+            # Remove meds that exceed target length
+            if len(new_med) <= med_len:
+                filtered_med_list.append(new_med)
+        set_new_med_list = set(filtered_med_list)
+        print(len(set_new_med_list))
+
+    return step
+
+
+def find_steps2(repl_list, target_med):
+    """Tries random replacements until 'e'. Works but not a good solution.
+    Does not work for test case 2."""
+
+    random.shuffle(repl_list)
+    final_med = target_med
+    count = 0
+    while final_med != 'e':
+        present_med = final_med
+        for repl, pat in repl_list:
+            if pat in final_med:
+                final_med = final_med.replace(pat, repl, 1)
+                # print(final_med)
+                count += 1
+        if present_med == final_med:
+            # Reinitialize the loop
+            print('Reinitializing')
+            # input('Continue?')
+            count = 0
+            random.shuffle(repl_list)
+            final_med = target_med
+        # print(final_med)
+
+    return count
+
+
 def test_read_file():
-    f_names = ['Day_19/19_test.txt',
-               'Day_19/19_test2.txt']
+    f_names = ['Day_19/19_test.txt', 'Day_19/19_test2.txt']
 
     for f in f_names:
         repl, med = read_file(f)
@@ -55,8 +110,7 @@ def test_repl_mol():
 
 def test_count_new_med():
     """Count the number of new medicines generated"""
-    f_names = ['Day_19/19_test.txt',
-               'Day_19/19_test2.txt']
+    f_names = ['Day_19/19_test.txt', 'Day_19/19_test2.txt']
 
     repl, med = read_file(f_names[0])
     new_med_list = replace_mol(repl, med)
@@ -67,45 +121,27 @@ def test_count_new_med():
     assert len(set(new_med_list)) == 7
 
 
+def test_find_steps():
+    repl_list, med = read_file(f_name='Day_19/19_test.txt')
+    repl_list.append(('e', 'H'))
+    repl_list.append(('e', 'O'))
+
+    step = find_steps2(repl_list, med)
+    assert step == 3
+
+
 def main():
+    start1 = time.time()
     repl_list, med = read_file()
     new_med_list = replace_mol(repl_list, med)
     print(len(set(new_med_list)))
+    print(f'Part 1 Time: {time.time() - start1}')
 
+    start2 = time.time()
+    steps = find_steps2(repl_list, med)
+    print(steps)
+    print(f'Part 2 Time: {time.time() - start2}')
 
-def main2():
-    # repl_list, med = read_file()
-    repl_list, med = read_file(f_name='Day_19/19_test.txt')
-    med_len = len(med)
-    step = 1
-    repl_list.append(('e', 'H'))
-    repl_list.append(('e', 'O'))
-    set_new_med_list = set(replace_mol(repl_list, 'e'))
-    print(set_new_med_list)
-
-    # for _ in range(3):
-    while med not in set_new_med_list:
-        step += 1
-        new_med_list = []
-
-        for new_med in set_new_med_list:
-            repl_med_list = replace_mol(repl_list, new_med)
-            new_med_list.extend(repl_med_list)
-
-        set_new_med_list = set(new_med_list)
-        print(set_new_med_list)
-
-        filtered_med_list = []
-        for new_med in set_new_med_list:
-            # Remove meds that exceed target length
-            if len(new_med) <= med_len:
-                filtered_med_list.append(new_med)
-        set_new_med_list = set(filtered_med_list)
-
-    print(step)
 
 if __name__ == '__main__':
-    start1 = time.time()
-    # main()
-    print(f'Part 1 finished in {time.time() - start1}')
-    main2()
+    main()
